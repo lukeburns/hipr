492eb7c1317b83f594af33f40542721335a34d46
Luke Burns <lukemburns@gmail.com>
2022-04-09 20:14:11 -0700

    default to unbound

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ module.exports = {
   RecursiveResolver: require('./RecursiveResolver'),
   AuthServer: require('./AuthServer'),
   RecursiveServer: require('./RecursiveServer'),
+  UnboundResolver: require('./UnboundResolver'),
+  UnboundServer: require('./UnboundServer'),
   ...require('./createDS')
 };
